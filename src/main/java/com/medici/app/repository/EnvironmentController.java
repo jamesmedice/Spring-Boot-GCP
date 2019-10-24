e9ce175f2e3a18d201d3d94bae135e952e03c4a4
jamesmedice <U0I2747 tiago.medice@gft.com>
2019-10-24 20:51:41 +0200

    refs

--- src/main/java/com/medici/app/repository/EnvironmentController.java
+++ src/main/java/com/medici/app/repository/EnvironmentController.java
@@ -1,12 +1,15 @@
 package com.medici.app.repository;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +28,16 @@ public class EnvironmentController {
 	Environment environment;
 
 	@RequestMapping(value = "/allVars", method = RequestMethod.GET)
-	public MutablePropertySources allVars() {
-		return ((AbstractEnvironment) environment).getPropertySources();
+	public Map<String, Object> allVars() {
+		Map<String, Object> map = new HashMap();
+		for (Iterator it = ((AbstractEnvironment) environment).getPropertySources().iterator(); it.hasNext();) {
+			PropertySource propertySource = (PropertySource) it.next();
+			if (propertySource instanceof MapPropertySource) {
+				map.putAll(((MapPropertySource) propertySource).getSource());
+			}
+		}
+
+		return map;
 	}
 
 	@RequestMapping(value = "/envVars", method = RequestMethod.GET)
