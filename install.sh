ad8a1f43d395b991ed3671e79355380dcc222ffc
jamesmedice <U0I2747 tiago.medice@gft.com>
2020-01-05 11:24:51 +0100

    new release

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 kubectl create secret generic mysqlcontainer --from-literal=username=james --from-literal=password=tpm1234
-kubectl get secret  mysqlcontainer -o yaml > secret.yaml
-kubectl apply  -f  secret.yaml
-
 kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=../key.json
